7afb2a64da4c882c0abda975a40b5f29c36503c5
kareem <skrym826@gmail.com>
2023-02-16 14:39:46 +0200

    updated test case,main and interactor class issues

--- src/test/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetHighestCarbonatedDrinksPricesInteractorTest.kt
@@ -1,7 +1,7 @@
 package interactor
 
 
-import model.CityEntity
+
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.TestInstance
 import dataSource.FakeDataSource
@@ -9,45 +9,56 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class GetHighestCarbonatedDrinksPricesInteractorTest{
+class GetHighestCarbonatedDrinksPricesInteractorTest {
 
-    private lateinit var fakeData : FakeDataSource
+    private lateinit var fakeData: FakeDataSource
     private lateinit var getHighestCarbonatedDrinksPricesInteractor: GetHighestCarbonatedDrinksPricesInteractor
+
     @BeforeAll
-    fun first(){
+    fun first() {
         fakeData = FakeDataSource()
         getHighestCarbonatedDrinksPricesInteractor = GetHighestCarbonatedDrinksPricesInteractor(fakeData)
     }
 
     @Test
-    fun should_return_listOfPairs_When_Data_Valid(){
+    fun should_return_listOfPairs_When_Data_Valid() {
         //given
         fakeData.setDataType(FakeDataSource.DataType.VALID)
         //when
-        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute()
+        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute(10)
         //then
-        assertTrue(listOfCountries.toSet().size ==10)
+        assertTrue(listOfCountries.toSet().size == 10)
     }
 
     @Test
-    fun should_return_empty_When_Data_LowQuality(){
+    fun should_return_empty_When_Data_LowQuality() {
         //given
         fakeData.setDataType(FakeDataSource.DataType.LOWQUALITY)
         //when
-        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute()
+        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute(10)
         //then
         assertTrue(listOfCountries.isEmpty())
     }
 
     @Test
-    fun should_return_empty_List_When_Data_NotFound(){
+    fun should_return_empty_List_When_Data_NotFound() {
         //given
         fakeData.setDataType(FakeDataSource.DataType.NULLABLE)
 
         //when
-        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute()
+        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute(10)
         //then return empty list
         assertTrue(listOfCountries.isEmpty())
     }
 
+    @Test
+    fun should_return_list_of_available_countries_when_limit_is_greater_than_number_of_countries_available() {
+        //given
+        fakeData.setDataType(FakeDataSource.DataType.VALID)
+        //when
+        val limit=99999
+        val listOfCountries = getHighestCarbonatedDrinksPricesInteractor.execute(limit)
+        //then
+        assertTrue(listOfCountries.size<limit)
+    }
 }
