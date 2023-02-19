7b449f86096e39496e84d321ac1334270d7b2f24
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-02-19 11:48:10 +0200

    add limit

--- src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
+++ src/main/kotlin/interactor/GetCheapestBananaPricesCitiesNamesInteractor.kt
@@ -1,19 +1,28 @@
 package interactor
 
+import interactor.GetCheapestBananaPricesCitiesNamesInteractor.Const.DEFAULT_LIMIT
 import model.CityEntity
 
-class  GetCheapestBananaPricesCitiesNamesInteractor(
+class GetCheapestBananaPricesCitiesNamesInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
     fun getCitiesVarArgs(): List<CityEntity> {
         return dataSource.getAllCitiesData()
     }
 
-    fun execute(vararg cityEntities: CityEntity): List<String> {
+    fun execute(
+        limit: Int = DEFAULT_LIMIT,
+        vararg cityEntities: CityEntity
+    ): List<String> {
         return cityEntities
             .filter { it.fruitAndVegetablesPrices.banana1kg != null }
             .sortedBy { it.fruitAndVegetablesPrices.banana1kg }
+            .take(limit)
             .map { it.cityName }
             .takeIf { it.isNotEmpty() } ?: listOf("No Valid Data is Entered !")
+    }
+
+    object Const {
+        const val DEFAULT_LIMIT = 10
     }
 }
