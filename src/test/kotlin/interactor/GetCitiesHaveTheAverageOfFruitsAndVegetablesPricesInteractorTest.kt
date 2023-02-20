1ff27712167d94151b7140dcf8d978d83b78265e
Menna Akram <mennaakram976@gmail.com>
2023-02-20 19:07:13 +0200

    test: add two test cases

--- src/test/kotlin/interactor/GetCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractorTest.kt
+++ src/test/kotlin/interactor/GetCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractorTest.kt
@@ -18,34 +18,50 @@ internal class GetCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractorTest{
         getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor =
             GetCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor(fakeData)
     }
+
     @Test
     fun should_ReturnTheTenCitiesName_When_TheDataIsValid() {
-        //given
+        //given the valid data and limit the result by 10
+        val limit = 10
         fakeData.setDataType(FakeDataSource.DataType.VALID)
-        //When
+        //When the limit equal to size of existing data
+        val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(limit)
+        //Then return true if the result is list of ten cities names
+        assertTrue(result.size == limit)
+    }
+
+    @Test
+    fun should_ReturnEmptyList_When_TheDataIsNull() {
+        //given null data
+        fakeData.setDataType(FakeDataSource.DataType.NULLABLE)
+        //When the result is empty
         val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(10)
-        //Then
-        assertTrue(result.size == 10)
+        //Then return true if the result is empty
+        assertTrue(result.isEmpty())
     }
 
     @Test
     fun should_ReturnTheTenCitiesName_When_TheDataIsMix() {
-        //given
+        //given mixed data and limit the result by 10
+        val limit = 10
         fakeData.setDataType(FakeDataSource.DataType.MIXTURE)
-        //When
-        val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(10)
-        //Then
-        assertEquals(result.size == 10 , result.size == 6)
+        //When the limit equal to size of existing data
+        val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(limit)
+        //Then return true if the result is list of ten cities names
+        assertTrue(result.size == limit)
     }
 
     @Test
-    fun should_ReturnTheTenCitiesName_When_TheDataIsNull() {
-        //given
-        fakeData.setDataType(FakeDataSource.DataType.NULLABLE)
-        //When
-        val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(10)
-        //Then
-        assertTrue(result.isEmpty())
+    fun should_ReturnEmptyList_When_EnterNotEnoughData() {
+        //given the valid Data and limit the result by 30
+        val limit = 30
+        fakeData.setDataType(FakeDataSource.DataType.VALID)
+        //When limit is more than the size of existing data
+        val result = getCitiesHaveTheAverageOfFruitsAndVegetablesPricesInteractor.execute(limit)
+        //Then return emptyList
+        assertEquals(emptyList<String>(), result)
     }
+
+
 
 }
